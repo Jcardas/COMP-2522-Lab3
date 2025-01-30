7cd87d81de8b96a394c2091999885a8aac3c64be
Jcardas <justincardas@gmail.com>
2025-01-29 20:21:52 -0800

    Complete IPhone class

--- src/Code/IPhone.java
+++ src/Code/IPhone.java
@@ -1,3 +1,10 @@
+import java.util.Objects;
+
+/**
+ * Represents an iPhone, a type of {@link IDevice} with the purpose of  talking.
+ * This class has variables for the phone's carrier and
+ * remaining minutes on its phone plan.
+ */
 public class IPhone extends IDevice
 {
 
@@ -13,9 +20,19 @@ public class IPhone extends IDevice
     private final double phonePlanMinutesRemaining;
     private final String carrier;
 
+    /**
+     * Constructs an {@link IPhone} object with the specified remaining minutes
+     * and carrier.
+     *
+     * @param phonePlanMinutesRemaining The number of minutes remaining
+     *                                  on the phone plan.
+     * @param carrier                   The mobile carrier for this {@link IPhone}.
+     * @throws IllegalArgumentException If phone plan minutes are less than {@code MIN_PHONE_PLAN_MINUTES_REMAINING}
+     *                                  or if the carrier is {@code null}.
+     */
     IPhone(final double phonePlanMinutesRemaining, final String carrier)
     {
-        super("talking");
+        super(PURPOSE);
         validatePhonePlanMinutesRemaining();
         validateCarrier();
 
@@ -23,33 +40,82 @@ public class IPhone extends IDevice
         this.carrier = carrier;
     }
 
+    /*
+     * Validates that the remaining phone plan minutes are not below
+     * the allowed minimum.
+     */
     private void validatePhonePlanMinutesRemaining()
     {
-        if(phonePlanMinutesRemaining < MIN_PHONE_PLAN_MINUTES_REMAINING)
+        if (phonePlanMinutesRemaining < MIN_PHONE_PLAN_MINUTES_REMAINING)
         {
             throw new IllegalArgumentException("Phone plan minutes remaining cannot be less than" +
                     MIN_PHONE_PLAN_MINUTES_REMAINING);
         }
     }
 
+    //     Validates that the carrier is not null.
     private void validateCarrier()
     {
-        if(carrier == null)
+        if (carrier == null)
         {
             throw new IllegalArgumentException("Carrier cannot be null.");
         }
     }
 
+    /**
+     * Returns a string representation of the {@link IPhone}, including its
+     * remaining phone plan minutes and carrier.
+     *
+     * @return A formatted string containing the {@link IPhone}'s details.
+     */
     @Override
     public String toString()
     {
-        return(super.toString() + ", it has " + phonePlanMinutesRemaining +
+        return (super.toString() + ", it has " + phonePlanMinutesRemaining +
                 " phone plan minutes remaining with the carrier " + carrier + ".");
     }
 
-    @Override public void printDetails()
+    @Override
+    public void printDetails()
     {
         System.out.println(toString());
     }
 
+    /**
+     * Checks if this IPhone object is equal to another object.
+     * Two IPhone objects are considered equal if they have the same
+     * amount of remaining phone plan minutes.
+     *
+     * @param obj The object to compare to this IPhone.
+     * @return {@code true} if the given object is an IPhone with the same
+     * remaining phone plan minutes, otherwise {@code false}.
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        boolean comparingIPhones;
+
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+        if (this == obj)
+            return true;
+        IPhone otherIPhone = (IPhone) obj;
+        comparingIPhones = this.phonePlanMinutesRemaining ==
+                otherIPhone.phonePlanMinutesRemaining;
+        return comparingIPhones;
+    }
+
+    /**
+     * Generates a hash code for this {@link IPhone} object.
+     * Ensures that equal objects have the same hash code.
+     *
+     * @return The hash code value for this {@link IPhone}.
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(phonePlanMinutesRemaining);
+    }
 }
