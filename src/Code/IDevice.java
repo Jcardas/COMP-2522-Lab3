8433399209b77bbae72bf6e73d3a5a3919841d5d
Grace Jung <gjung20@my.bcit.ca>
2025-01-29 18:20:55 -0800

    basic framework for iDevice class done. iPod class basically done.

--- src/Code/IDevice.java
+++ src/Code/IDevice.java
@@ -1,3 +1,35 @@
-public class IDevice
+/**
+ * Represents a device.
+ * A device gets it's purpose and prints its own details.
+ */
+public abstract class IDevice
 {
+    private final String purpose;
+
+    public IDevice()
+    {
+        this.purpose = "";
+    }
+
+    public IDevice(String purpose)
+    {
+        this.purpose = purpose;
+
+    }
+
+    public String getPurpose()
+    {
+        String purpose;
+
+        purpose = "";
+
+        return purpose;
+    }
+
+    private void validatePurpose()
+    {
+
+    }
+
+    public abstract void printDetails();
 }
--- src/Code/IPod.java
+++ src/Code/IPod.java
@@ -1,3 +1,151 @@
-public class IPod
+import java.util.Objects;
+
+/**
+ * Represents a device you can listen to music on, which is a subclass of {@link IDevice}.
+ * Contains instance variables, constructor parameters,
+ * accessor methods, and mutator methods for values:
+ * (int) number of songs stored, and
+ * (double) maximum volume in decibels.
+ * Overrides the toString() method to return all the instance variables
+ * in a single String.
+ * Overrides the equals() method. IPods with the same number
+ * of songs stored are considered equal.
+ * Override the hashcode() method whenever you override equals().
+ *
+ * @author grace
+ * @author justin
+ * @version 1.0
+ */
+public class IPod extends IDevice
 {
+    private static final double MIN_DECIBEL_LVL = 0.0;
+    private static final double UNSAFE_DECIBEL_LVL = 85.0;
+
+    int numOfStoredSongs;
+    final double maxVolumeDecibel;
+
+    /**
+     * Constructs an iPod object.
+     *
+     * @param numOfStoredSongs the current number of stored songs on the iPod.
+     * @param maxVolumeDecibel the absolute max volume the iPod
+     *                         can go measured in decibels.
+     */
+    public IPod(int numOfStoredSongs, double maxVolumeDecibel)
+    {
+        this.numOfStoredSongs = numOfStoredSongs;
+        this.maxVolumeDecibel = maxVolumeDecibel;
+
+        validateMaxDecibelLevel();
+    }
+
+    /**
+     * Prints the numOfStoredSongs and maxVolumeDecibel as a string.
+     */
+    @Override
+    public void printDetails()
+    {
+        System.out.println(toString());
+    }
+
+    /**
+     * @return the number of songs stored in the iPod as an integer value.
+     */
+    public int getNumOfStoredSongs() {
+        return numOfStoredSongs;
+    }
+
+    /**
+     *
+     * @param numOfStoredSongs is the number of songs stored on
+     *                         the iPod currently.
+     */
+    public void setNumOfStoredSongs(int numOfStoredSongs) {
+        this.numOfStoredSongs = numOfStoredSongs;
+    }
+
+    /**
+     * Adds 1 to the count of stored songs.
+     */
+    public void addSong()
+    {
+        numOfStoredSongs++;
+    }
+
+    /**
+     * Lowers the number of stored songs by 1.
+     */
+    public void delSong()
+    {
+        numOfStoredSongs--;
+    }
+
+    /**
+     * Validates the inputted decibel level and
+     * makes sure it's between 0.0 db and
+     * 85.0 db (the level when volume become dangerous for humans).
+     */
+    public void validateMaxDecibelLevel()
+    {
+        if(maxVolumeDecibel < MIN_DECIBEL_LVL)
+        {
+            throw new IllegalArgumentException("Max volume must be a positive number.");
+        } else if(maxVolumeDecibel > UNSAFE_DECIBEL_LVL)
+        {
+            throw new IllegalArgumentException("Please input a number that is a safe decibel number.");
+        }
+    }
+
+    /**
+     * Returns the numOfStoredSongs and
+     * maxVolumeDecibel as a single string.
+     *
+     * @return the number of stored songs and max volume
+     *         as a string.
+     */
+    @Override
+    public String toString()
+    {
+        StringBuilder sb;
+        sb = new StringBuilder();
+
+        sb.append(String.format(
+                "Number of stored songs: %d\n", numOfStoredSongs));
+        sb.append(String.format(
+                "Max volume decibel: %f\n", maxVolumeDecibel));
+
+        return sb.toString();
+    }
+
+    /**
+     * Compares how many songs two iPod objects
+     * have in comparison to each other.
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        boolean comparingIPods;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        if (this == obj)
+            return true;
+        IPod other = (IPod) obj;
+        comparingIPods = this.numOfStoredSongs == other.numOfStoredSongs;
+
+        return comparingIPods;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                numOfStoredSongs,
+                maxVolumeDecibel);
+    }
 }
